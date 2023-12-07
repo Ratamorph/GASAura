37bd3cb4dc39c562bc8b388e6a17fd61122751b7
Ratamorph <lcorrea@c2gamestudio.com>
2023-12-07 16:29:09 -0500

    Effect actor and potion

---
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,49 @@
+// Luis Correa
+
+
+#include "Actor/AuraEffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AAuraEffectActor::AAuraEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	Trigger = CreateDefaultSubobject<USphereComponent>("Trigger");
+
+	SetRootComponent(Mesh);
+	Trigger->SetupAttachment(GetRootComponent());
+}
+
+void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(IAbilitySystemInterface * ASCInterface =  Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const UAuraAttributeSet *AttributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+
+		UAuraAttributeSet *MutableAuraAttributeSet = const_cast<UAuraAttributeSet *>(AttributeSet);
+		MutableAuraAttributeSet->SetHealth(MutableAuraAttributeSet->GetHealth() + 25);
+		Destroy();
+	}
+}
+
+void AAuraEffectActor::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+// Called when the game starts or when spawned
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	Trigger->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
+	Trigger->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::OnEndOverlap);
+}
+
+
